297fbb5ff14d5c2f258afd44977574ca11c2bcaf
wx-5504 <wx-yangmy@dtdream.com>
2018-05-09 18:25:38 +0800

    webpack+vue+express例子

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <p class="testData">
+      {{ testData }}
+    </p>
+    <!--<router-view/>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      testData: '111'
+    }
+  },
+  mounted () {
+    let self = this
+    axios.get('/api/test/getData')
+      .then((response) => {
+        self.testData = response.data.result.value
+      })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
